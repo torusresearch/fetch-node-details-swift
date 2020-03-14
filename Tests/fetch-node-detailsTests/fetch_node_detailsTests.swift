91d32dd35f90ff3c7d63de1407c97d900d512c84
shubham <shubhamrathi@icloud.com>
2020-03-14 19:55:26 +0800

    checkpoint before changing web3 library

--- Tests/fetch-node-detailsTests/fetch_node_detailsTests.swift
+++ Tests/fetch-node-detailsTests/fetch_node_detailsTests.swift
@@ -6,8 +6,10 @@ final class fetch_node_detailsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        FetchNodeDetails()
-//        XCTAssertEqual(fetch_node_details().text, "Hello, World!")
+        var fnd = FetchNodeDetails()
+        let res = fnd.getCurrentEpoch()
+        print(res)
+        //        XCTAssertEqual(fetch_node_details().text, "Hello, World!")
     }
 
     static var allTests = [
