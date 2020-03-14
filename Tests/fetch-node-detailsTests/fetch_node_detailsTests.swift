61eba10b93ee8e9d42033c38a158ab07d4b4542d
shubham <shubhamrathi@icloud.com>
2020-03-14 17:53:06 +0800

    contract issue resolved

--- Tests/fetch-node-detailsTests/fetch_node_detailsTests.swift
+++ Tests/fetch-node-detailsTests/fetch_node_detailsTests.swift
@@ -6,6 +6,7 @@ final class fetch_node_detailsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
+        FetchNodeDetails()
 //        XCTAssertEqual(fetch_node_details().text, "Hello, World!")
     }
 
