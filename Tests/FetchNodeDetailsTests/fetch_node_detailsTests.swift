94e8385fafe8ecd52465043c12e9da38a98f7fa2
rathishubham7 <shubhamrathi@icloud.com>
2021-08-10 09:11:03 +0530

    feat: fixed tests, removed comments;

--- Tests/FetchNodeDetailsTests/fetch_node_detailsTests.swift
+++ Tests/FetchNodeDetailsTests/fetch_node_detailsTests.swift
@@ -13,11 +13,11 @@ final class fetch_node_detailsTestsAsync: XCTestCase{
         let exp = expectation(description: "sample")
         let fnd = FetchNodeDetails(proxyAddress: "0x638646503746d5456209e33a2ff5e3226d698bea", network: .MAINNET)
         
-        let EpochInfo = EpochInfo(id: BigInt("19", radix: 10)!, n: BigInt("9", radix: 10)!, k: BigInt("5", radix: 10)!, t: BigInt("2", radix: 10)!, nodeList: [], prevEpoch: BigInt("17", radix: 10)!, nextEpoch: BigInt("19", radix: 10)!)
-        let epochToCheck = BigInt(10)
+//        let EpochInfo = EpochInfo(id: BigInt("19", radix: 10)!, n: BigInt("9", radix: 10)!, k: BigInt("5", radix: 10)!, t: BigInt("2", radix: 10)!, nodeList: [], prevEpoch: BigInt("17", radix: 10)!, nextEpoch: BigInt("19", radix: 10)!)
+//        let epochToCheck = BigInt(10)
         
-        try! fnd.getCurrentEpochPromise().done{ data in
-            print(data)
+        fnd.getCurrentEpochPromise().done{ data in
+            XCTAssertNotNil(data)
             exp.fulfill()
         }.catch{ error in
             XCTFail()
@@ -33,8 +33,7 @@ final class fetch_node_detailsTestsAsync: XCTestCase{
         let EpochInfo = EpochInfo(id: BigInt("19", radix: 10)!, n: BigInt("9", radix: 10)!, k: BigInt("5", radix: 10)!, t: BigInt("2", radix: 10)!, nodeList: [], prevEpoch: BigInt("17", radix: 10)!, nextEpoch: BigInt("19", radix: 10)!)
         let epochToCheck = BigInt(19)
         
-        try! fnd.getEpochInfoPromise(epoch: epochToCheck).done{ data in
-            print(data)
+        fnd.getEpochInfoPromise(epoch: epochToCheck).done{ data in
             XCTAssertEqual(data.id, EpochInfo.id)
             exp.fulfill()
         }.catch{ error in
@@ -47,8 +46,7 @@ final class fetch_node_detailsTestsAsync: XCTestCase{
         let exp = expectation(description: "Should be ablt to get node info")
         let fnd = FetchNodeDetails(proxyAddress: "0x638646503746d5456209e33a2ff5e3226d698bea", network: .MAINNET)
         
-        try! fnd.getNodeDetails(nodeEthAddress: "0x58DF12150e765cFa08d3d7027FFeFe3EBC6a977d").done{ data in
-            print(data)
+        fnd.getNodeDetails(nodeEthAddress: "0x58DF12150e765cFa08d3d7027FFeFe3EBC6a977d").done{ data in
             exp.fulfill()
         }.catch{ error in
             XCTFail()
@@ -57,15 +55,14 @@ final class fetch_node_detailsTestsAsync: XCTestCase{
     }
     
     func test_getAllNodeDetails(){
-        let exp = expectation(description: "Should be ablt to get node info")
+        let exp = expectation(description: "Should be able to get node info")
         let fnd = FetchNodeDetails(proxyAddress: "0x638646503746d5456209e33a2ff5e3226d698bea", network: .MAINNET)
         
-        try! fnd.getAllNodeDetails().done{ data in
-            print(data)
+        fnd.getAllNodeDetails().done{ data in
             exp.fulfill()
         }.catch{ error in
             XCTFail()
         }
-        wait(for: [exp], timeout: 5)
+        wait(for: [exp], timeout: 10)
     }
 }
