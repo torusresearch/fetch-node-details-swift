551eaa7e388e565c2b7456e360aa27ed32ff3586
dhruv <dhruvjaiswal5@gmail.com>
2022-06-06 23:09:01 +0530

    web3 ver bump

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["FetchNodeDetails"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/argentlabs/web3.swift", from:"0.8.1"),
+        .package(name:"web3.swift", url: "https://github.com/argentlabs/web3.swift", from:"0.9.3"),
         .package(url: "https://github.com/mxcl/PromiseKit", from: "6.0.0")
     ],
     targets: [
