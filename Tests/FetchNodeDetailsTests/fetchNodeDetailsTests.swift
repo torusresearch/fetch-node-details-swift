8a9ca60bd5ab83f3293ab200bb8059ea768c1240
dhruv <dhruvjaiswal5@gmail.com>
2022-06-13 13:32:42 +0530

    async await support added

--- Tests/FetchNodeDetailsTests/fetchNodeDetailsTests.swift
+++ Tests/FetchNodeDetailsTests/fetchNodeDetailsTests.swift
@@ -7,51 +7,43 @@ import XCTest
 class fetchNodeDetailsTests: XCTestCase {
     var timeout: TimeInterval = 10
 
-    func test_getNode_MainNet() {
-        let exp = expectation(description: "should get node details")
+    func test_getNode_MainNet() async {
         let fnd = FetchNodeDetails()
-        fnd.getNodeDetails(verifier: "google", verifierID: "hello@tor.us").done { result in
+        do {
+            let result = try await fnd.getNodeDetails(verifier: "google", verifierID: "hello@tor.us")
             XCTAssertEqual(result, SampleOutputMainNet(proxyAddress: "0xf20336e16B5182637f09821c27BDe29b0AFcfe80").val)
-            exp.fulfill()
-        }.catch { _ in
-            XCTFail()
+        } catch {
+            XCTFail(error.localizedDescription)
         }
-        wait(for: [exp], timeout: timeout)
     }
 
-    func test_getNode_custom_path_MainNet() {
-        let exp = expectation(description: "should get node details")
+    func test_getNode_custom_path_MainNet() async {
         let fnd = FetchNodeDetails(network: .CUSTOM(path: "https://\("mainnet").infura.io/v3/\("7f287687b3d049e2bea7b64869ee30a3")"))
-        fnd.getNodeDetails(verifier: "google", verifierID: "hello@tor.us").done { result in
+        do {
+            let result = try await fnd.getNodeDetails(verifier: "google", verifierID: "hello@tor.us")
             XCTAssertEqual(result, SampleOutputMainNet(proxyAddress: "0xf20336e16B5182637f09821c27BDe29b0AFcfe80").val)
-            exp.fulfill()
-        }.catch { _ in
-            XCTFail()
+        } catch {
+            XCTFail(error.localizedDescription)
         }
-        wait(for: [exp], timeout: timeout)
     }
 
-    func test_getNode_Ropsten() {
-        let exp = expectation(description: "should get node details")
+    func test_getNode_Ropsten() async {
         let fnd = FetchNodeDetails(proxyAddress: "0x6258c9d6c12ed3edda59a1a6527e469517744aa7", network: .ROPSTEN)
-        fnd.getNodeDetails(verifier: "google", verifierID: "hello@tor.us").done { result in
+        do {
+            let result = try await fnd.getNodeDetails(verifier: "google", verifierID: "hello@tor.us")
             XCTAssertEqual(result, SampleOutputRopsten(proxyAddress: "0x6258c9d6c12ed3edda59a1a6527e469517744aa7").val)
-            exp.fulfill()
-        }.catch { _ in
-            XCTFail()
+        } catch {
+            XCTFail(error.localizedDescription)
         }
-        wait(for: [exp], timeout: timeout)
     }
 
-    func test_getNode_Polygon() {
-        let exp = expectation(description: "should get node details")
+    func test_getNode_Polygon() async {
         let fnd = FetchNodeDetails(proxyAddress: "0x9f072ba19b3370e512aa1b4bfcdaf97283168005", network: .POLYGON)
-        fnd.getNodeDetails(verifier: "google", verifierID: "hello@tor.us").done { result in
+        do {
+            let result = try await fnd.getNodeDetails(verifier: "google", verifierID: "hello@tor.us")
             XCTAssertEqual(result, SampleOutputPolygon(proxyAddress: "0x9f072ba19b3370e512aa1b4bfcdaf97283168005").val)
-            exp.fulfill()
-        }.catch { _ in
-            XCTFail()
+        } catch {
+            XCTFail(error.localizedDescription)
         }
-        wait(for: [exp], timeout: timeout)
     }
 }
