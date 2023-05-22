231feeb9ebe83b8afd472527f9b55a0ef500810a
hqjang-pepper <hqjang95@gmail.com>
2023-05-22 23:34:02 +0900

    adjust properties of Node details

--- Sources/FetchNodeDetails/Models/AllNodeDetailsModel.swift
+++ Sources/FetchNodeDetails/Models/AllNodeDetailsModel.swift
@@ -7,15 +7,19 @@ public struct AllNodeDetailsModel:Equatable {
     }
     
     private var currentEpoch : BigUInt
-    private var nodeListAddress : String
     private var torusNodeEndpoints : Array<String>
+    private var torusNodeSSSEndpoints : Array<String>
+    private var torusNodeRSSEndpoints : Array<String>
+    private var torusNodeTSSEndpoints : Array<String>
     private var torusIndexes : Array<BigUInt>
     private var torusNodePub : Array<TorusNodePubModel>
     private var updated = false
     
-    public init(_currentEpoch : BigUInt, _nodeListAddress : String, _torusNodeEndpoints : Array<String>,  _torusIndexes : Array<BigUInt>, _torusNodePub : Array<TorusNodePubModel>, _updated : Bool) {
+    public init(_currentEpoch : BigUInt, _torusNodeEndpoints : Array<String>, _torusNodeSSSEndpoints : Array<String>, _torusNodeRSSEndpoints : Array<String>, _torusNodeTSSEndpoints : Array<String>,  _torusIndexes : Array<BigUInt>, _torusNodePub : Array<TorusNodePubModel>, _updated : Bool) {
         self.currentEpoch = _currentEpoch;
-        self.nodeListAddress = _nodeListAddress;
+        self.torusNodeSSSEndpoints = _torusNodeSSSEndpoints;
+        self.torusNodeRSSEndpoints = _torusNodeRSSEndpoints;
+        self.torusNodeTSSEndpoints = _torusNodeTSSEndpoints;
         self.torusNodeEndpoints = _torusNodeEndpoints;
         self.torusIndexes = _torusIndexes;
         self.torusNodePub = _torusNodePub;
@@ -46,14 +50,6 @@ public struct AllNodeDetailsModel:Equatable {
         self.currentEpoch = currentEpoch;
     }
     
-    public func getNodeListAddress() -> String {
-        return nodeListAddress
-    }
-    
-    public mutating func setNodeListAddress(nodeListAddress : String) {
-        self.nodeListAddress = nodeListAddress
-    }
-    
     public func getTorusNodeEndpoints() ->  Array<String> {
         return torusNodeEndpoints
     }
@@ -68,5 +64,48 @@ public struct AllNodeDetailsModel:Equatable {
     
     public mutating func setTorusNodePub(torusNodePub : Array<TorusNodePubModel>) {
         self.torusNodePub = torusNodePub
+    }
+    
+    public func getTorusNodeSSSEndpoints() ->  Array<String> {
+        return torusNodeSSSEndpoints
+    }
+    
+    public mutating func setTorusNodeSSSEndpoints(torusNodeSSSEndpoints : Array<String>) {
+        self.torusNodeSSSEndpoints = torusNodeSSSEndpoints
+    }
+    
+    public func getTorusNodeRSSEndpoints() ->  Array<String> {
+        return torusNodeRSSEndpoints
+    }
+    
+    public mutating func setTorusNodeRSSEndpoints(torusNodeRSSEndpoints : Array<String>) {
+        self.torusNodeRSSEndpoints = torusNodeRSSEndpoints
+    }
+    
+    public func getTorusNodeTSSEndpoints() ->  Array<String> {
+        return torusNodeTSSEndpoints
+    }
+    
+    public mutating func setTorusNodeTSSEndpoints(torusNodeTSSEndpoints : Array<String>) {
+        self.torusNodeTSSEndpoints = torusNodeTSSEndpoints
+    }
+    
+    public mutating func setNodeDetails(nodeDetails: AllNodeDetailsModel) {
+        let currentEpoch = nodeDetails.currentEpoch
+        let torusNodeEndpoints = nodeDetails.torusNodeEndpoints
+        let torusNodeSSSEndpoints = nodeDetails.torusNodeSSSEndpoints
+        let torusNodeRSSEndpoints = nodeDetails.torusNodeRSSEndpoints
+        let torusNodeTSSEndpoints = nodeDetails.torusNodeTSSEndpoints
+        let torusNodePub = nodeDetails.torusNodePub
+        let torusIndexes = nodeDetails.torusIndexes
+
+        self.torusNodeEndpoints = torusNodeEndpoints
+        self.torusNodeSSSEndpoints = torusNodeSSSEndpoints
+        self.torusNodeRSSEndpoints = torusNodeRSSEndpoints
+        self.torusNodeTSSEndpoints = torusNodeTSSEndpoints
+        self.torusNodePub = torusNodePub
+        self.torusIndexes = torusIndexes
+        self.currentEpoch = currentEpoch
+        self.updated = true
     }
 }
