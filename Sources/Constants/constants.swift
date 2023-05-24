f6dca6c59783c476006d3c2159cdee89111caebc
hqjang-pepper <hqjang95@gmail.com>
2023-05-24 20:51:16 +0900

    delete contract files & restructure directory

--- Sources/Constants/constants.swift
+++ Sources/Constants/constants.swift
@@ -10,13 +10,6 @@ let TORUS_NETWORK: [TorusNetwork: String] = [
 ]
 public typealias TORUS_NETWORK_TYPE = String
 
-let PROXY_CONTRACT_ADDRESS: [TorusNetwork: String] = [
-    .MAINNET: "0xf20336e16B5182637f09821c27BDe29b0AFcfe80",
-    .TESTNET: "0xd084604e5FA387FbC2Da8bAab07fDD6aDED4614A",
-    .CYAN: "0x9f072ba19b3370e512aa1b4bfcdaf97283168005",
-    .AQUA: "0x29Dea82a0509153b91040ee13cDBba0f03efb625",
-    .CELESTE: "0x6Bffb4e89453069E7487f0fa5c9f4a2D771cce6c"
-]
 
 let MULTI_CLUSTER_NETWORKS: [TorusNetwork] = [.AQUA, .CELESTE, .CYAN]
 
