0034fbb4642c2d93ab82ca7a783cf89d16bec515
hqjang-pepper <hqjang95@gmail.com>
2023-05-30 17:20:58 +0900

    add bigint dependency

--- Package.swift
+++ Package.swift
@@ -15,12 +15,13 @@ let package = Package(
             targets: ["FndBase"])
     ],
     dependencies: [
-        .package(url: "https://github.com/argentlabs/web3.swift", from:"0.8.1")
+        .package(url: "https://github.com/argentlabs/web3.swift", from:"0.8.1"),
+        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
     ],
     targets: [
         .target(
             name: "CommonSources",
-            dependencies: [],
+            dependencies: ["BigInt"],
             path: "Sources/CommonSources"),
         .target(
             name: "FndBase",
