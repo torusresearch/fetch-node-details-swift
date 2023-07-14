5f3b021e64b2e29281fff828e8a09badf71ee3fc
ieow <cwlee0@gmail.com>
2023-07-14 10:55:25 +0800

    chore: cleanup

--- Sources/CommonSources/TorusNetwork.swift
+++ Sources/CommonSources/TorusNetwork.swift
@@ -1,36 +1,5 @@
 import Foundation
 
-//public enum TorusNetwork: Equatable, Hashable {
-//    case SAPPHIRE_DEVNET
-//    case SAPPHIRE_TESTNET
-//    case SAPPHIRE_MAINNET
-//    case MAINNET
-//    case TESTNET
-//    case CYAN
-//    case AQUA
-//    case CELESTE
-//    case CUSTOM(path: String)
-//
-//    public var path: String {
-//        switch self {
-//        case .SAPPHIRE_DEVNET:
-//            return "sapphire_devnet"
-//        case .SAPPHIRE_TESTNET:
-//            return "sapphire_testnet"
-//        case .SAPPHIRE_MAINNET:
-//            return "sapphire_mainnet"
-//        case .MAINNET:
-//            return "mainnet"
-//        case .TESTNET:
-//            return "goerli"
-//        case .CYAN, .AQUA, .CELESTE:
-//            return "polygon-mainnet"
-//        case let .CUSTOM(path):
-//            return path
-//        }
-//    }
-//}
-
 public enum TorusNetwork : Equatable, Hashable {
     case legacy(LegacyNetwork)
     case sapphire(SapphireNetwork)
@@ -117,6 +86,32 @@ public enum LegacyNetwork: Equatable, Hashable {
             return "celeste"
         case .CUSTOM(_):
             return "custom"
+        }
+    }
+    
+    public var migration_map: LegacyNetworkMigrationInfo {
+        return LegacyNetworkMigrationInfo(migrationCompleted: false, networkIdentifier: self.name, networkMigratedTo: SapphireNetwork.SAPPHIRE_MAINNET)
+    }
+    
+    public var networkMap : String {
+        switch self {
+        case .MAINNET: return "mainnet"
+        case .TESTNET: return "goerli"
+        case .CYAN: return "polygon-mainnet"
+        case .AQUA: return "polygon-mainnet"
+        case .CELESTE: return "polygon-mainnet"
+        case .CUSTOM(let path) : return path
+        }
+    }
+    
+    public var signerMap : String {
+        switch self {
+        case .MAINNET: return "https://signer.tor.us"
+        case .TESTNET: return "https://signer.tor.us"
+        case .CYAN: return "https://signer-polygon.tor.us"
+        case .AQUA: return "https://signer-polygon.tor.us"
+        case .CELESTE: return "https://signer-polygon.tor.us"
+        case .CUSTOM(let path) : return path
         }
     }
 }
--- Sources/FndBase/Utils/Utils.swift
+++ Sources/FndBase/Utils/Utils.swift
@@ -9,20 +9,5 @@ public func fetchLocalConfig(network: TorusNetwork) throws -> AllNodeDetailsMode
         case .sapphire(let network) :
             return try SapphireConfig(network: network)
     }
-//
-//    switch network {
-//        case .SAPPHIRE_DEVNET:
-//                return NODE_DETAILS_SAPPHIRE_DEVNET
-//        case .SAPPHIRE_TESTNET:
-//                return NODE_DETAILS_SAPPHIRE_TESTNET
-//        case .SAPPHIRE_MAINNET:
-//                return NODE_DETAILS_SAPPHIRE_MAINNET
-//        case .MAINNET, .CYAN, .AQUA, .CELESTE:
-//                return NODE_DETAILS_MAINNET
-//        case .TESTNET:
-//                return NODE_DETAILS_TESTNET
-//        default:
-//            return nil
-//    }
 }
 
