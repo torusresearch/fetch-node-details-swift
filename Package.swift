869c1288b5dbc772983f0beb86a36c32718e0c59
ieow <cwlee0@gmail.com>
2023-08-18 20:00:18 +0800

    fix: urlSession need macos 10.15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FetchNodeDetails",
-    platforms: [.iOS(.v13), .macOS(.v10_13)],
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         .library(
             name: "FetchNodeDetails",
