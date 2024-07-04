8c9d28d4cdf09ae4e217b2817f5baa508f874e21
metalurgical <97008724+metalurgical@users.noreply.github.com>
2024-07-04 21:31:13 +0200

    remove redundant error

--- Sources/CommonSources/Error.swift
+++ Sources/CommonSources/Error.swift
@@ -10,7 +10,6 @@ import Foundation
 public enum FetchNodeError : Error {
     case InvalidURL
     case InvalidInput
-    case InvalidAllNodeDetailsModel
     case GeneralError(String)
     case InvalidNetwork(String)
     case InvalidMigrationNetwork(String)
